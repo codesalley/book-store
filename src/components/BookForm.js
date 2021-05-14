c7708e23d907c48f5c71539270a14aabb19891ff
juxsalley <juxsalley@gmail.com>
2021-05-14 09:51:20 +0000

    import font

--- src/components/BookForm.js
+++ src/components/BookForm.js
@@ -1,29 +1,31 @@
 import CATEGORIES from '../utils/categories';
+import './BookForm.css';
 
 const BookForm = () => (
-  <div>
+  <div className="form-div">
     <form>
 
-      <label htmlFor="title" className="form-label">
+      <div className="form-label">
         <p> Book Title</p>
         <input id="title" type="text" className="form-control" placeholder="eg Dr, Wahab" />
-      </label>
+      </div>
 
-      <label htmlFor="author" className="form-label">
+      <div className="form-label">
         <p> Book Author</p>
         <input id="author" type="text" className="form-control" placeholder="eg Avengers" />
-      </label>
-      <label htmlFor="author" className="form-label">
+      </div>
+      <div className="form-label">
         <p> Release Year</p>
         <input id="author" type="date" className="form-control" placeholder="eg Avengers" />
-      </label>
+      </div>
 
-      <label htmlFor="category" className="form-label">
+      <div className="form-label">
         <p>  Book Category </p>
         <select id="category">
           {CATEGORIES.map((ele, index) => <option key={index.toString()}>{ele}</option>)}
         </select>
-      </label>
+      </div>
+      <button className="btn" type="submit">Add Book</button>
 
     </form>
   </div>
